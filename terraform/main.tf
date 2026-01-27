549ab94c017e238a0b7226268c586e2ca613e236
brayan.parra <brayanpp2697@gmail.com>
2026-01-27 14:04:27 -0500

    fix: fix terraform.tfstate

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "ms-franchise-neg-tfstate"
-    key            = "dev/terraform.tfstate"
+    key            = "terraform.tfstate"
     region         = "us-east-2"
     dynamodb_table = "terraform-locks"
     encrypt        = true
